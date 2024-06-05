bc084c7e69da45c3f4aa6210083318c1af78ee7e
nisslyak <nissopodolyak@gmail.com>
2024-06-05 07:12:25 +0300

    First commit

---
+++ Solution/Solution/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public static int MajorityElement(int[] nums)
+    {
+
+        var z = nums.GroupBy(i => i)
+            .OrderByDescending(group => group.Count())
+            .Select(group => group.Key).First();
+        return 0;
+    }
+
+    public static void Main(string[] args)
+    {
+        var array = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+
+        int majorityElement = MajorityElement((int[])array);
+    }
+}
